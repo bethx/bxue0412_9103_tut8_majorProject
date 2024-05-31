75520143c9f5ac416ab1520000784489d08ca27c
bethx <168094529+bethx@users.noreply.github.com>
2024-05-31 18:35:32 +1000

    More comments

--- sketch.js
+++ sketch.js
@@ -1,16 +1,18 @@
+// variables from group code
 let circleDiameter;
 let circles = []; // Array for big circles
-let redCircles = []; // Array for small red circles
-let redCircleAmount = 400;
+let bgCircles = []; // Array for small red circles
+let bgCircleAmount = 400;
 
+// variables used to add noise
 var colourNoise;
 var wavyLineNoise;
 var nestedRingNoise;
 var smallCircleNoise;
 var xoff;
 
+// variables used to add outline effect to big circles
 let smallCircleDiameter;
-
 let circleSize;
 let targetCircleSize;
 const easing = 0.05;
@@ -37,7 +39,8 @@ let circleColors = [
   [253, 220, 62]
 ];
 
-// function to create the lines in the background, adapted from https://editor.p5js.org/zygugi/sketches/BJHK3O_yM
+/* function to create the lines in the background
+This code was adapted from https://editor.p5js.org/zygugi/sketches/BJHK3O_yM */
 function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB, radiusFactor) {
   noFill();
   stroke(lineR, lineG, lineB);
@@ -59,14 +62,15 @@ function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB, radiusFactor
   endShape();
 }
 
-// Class for small random red circles
-class RedCirclePattern {
+// Class for small random bcakground circles
+class bgCirclePattern {
   constructor(xPos, yPos, radius) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.radius = radius;
   }
 
+  // Individual code: added noise to the green and blue to make the 
   display() {
     let bgCircleG = 255 * noise(colourNoise+5);
     let bgCircleB = 5 * noise(colourNoise+5);
@@ -95,7 +99,6 @@ class CirclePattern {
     for (let i = 0; i < 5; i++) {
       this.additionalRingColors.push([random(0, 255), random(0, 255), random(0, 255)]);
     }
-    pop;
   }
 
   display() {
@@ -131,10 +134,12 @@ class CirclePattern {
     ];
     for (let i = 0; i < ringRadii.length; i++) {
       fill(this.additionalRingColors[i]); // Achieve different color fills by calling the data in the array
-      circle(x, y, ringRadii[i] * 3 * noise(nestedRingNoise + i));
+      circle(x, y, ringRadii[i] * 3 * noise(nestedRingNoise + i)); // Individual code: added noise to make the circle size change randomly
     }
   }
 
+  /*Creates small random circles inside the larger circle
+  This code was adapted from https://editor.p5js.org/slow_izzm/sketches/HyqLs-7AX */
   generateRandomSmallCircles() {
     let smallCircles = [];
     let x = this.xFactor * windowHeight / 20;
@@ -162,6 +167,7 @@ class CirclePattern {
     return smallCircles;
   }
 
+  // Checks if the small circles will overlap with others
   isValidPosition(newCircle, smallCircles, diameter) {
     for (let circle of smallCircles) {
       let distance = dist(newCircle.x, newCircle.y, circle.x, circle.y);
@@ -177,7 +183,8 @@ class CirclePattern {
     noStroke(); // Remove stroke
     for (let smallCircle of this.smallCircles) {
       fill(smallCircle.color);
-      circle(smallCircle.x, smallCircle.y, smallCircleDiameter * noise(smallCircleNoise));
+      circle(smallCircle.x, smallCircle.y, smallCircleDiameter * noise(smallCircleNoise)); 
+      //Individual code: added noise to the diameter so that they change size
     }
   }
 
@@ -193,6 +200,8 @@ class CirclePattern {
 function setup() {
   createCanvas(windowHeight, windowHeight);
   background(5, 89, 127);
+
+  // sets up variables to add noise
   wavyLineNoise = 0;
   colourNoise = 0;
   nestedRingNoise = 0;
@@ -200,6 +209,7 @@ function setup() {
 
   circleDiameter = (windowHeight / 20) * 5.5;
 
+  // sets up variables for big circle outline animation
   circleSize = circleDiameter;
   targetCircleSize = circleSize + 20;
 
@@ -208,18 +218,19 @@ function setup() {
     circles.push(new CirclePattern(wavylineX[i], wavylineY[i], circleColors[i]));
   }
 
-  // Create small red circles
-  for (let i = 0; i < redCircleAmount; i++) {
+  /* Create small background circles
+This code was adapted from https://www.youtube.com/watch?v=XATr_jdh-44&t=122s */
+  for (let i = 0; i < bgCircleAmount; i++) {
     let overlapping = true;
-    let redCircle;
+    let bgCircle;
     while (overlapping) {
       overlapping = false;
-      redCircle = new RedCirclePattern(random(width), random(height), random(0, 10));
+      bgCircle = new bgCirclePattern(random(width), random(height), random(0, 10));
 
       // Check for overlap with other small red circles
-      for (let other of redCircles) {
-        let d = dist(redCircle.xPos, redCircle.yPos, other.xPos, other.yPos);
-        if (d < redCircle.radius + other.radius) {
+      for (let other of bgCircles) {
+        let d = dist(bgCircle.xPos, bgCircle.yPos, other.xPos, other.yPos);
+        if (d < bgCircle.radius + other.radius) {
           overlapping = true;
           break;
         }
@@ -227,38 +238,44 @@ function setup() {
 
       // Check for overlap with big circles
       for (let bigCircle of circles) {
-        let d = dist(redCircle.xPos, redCircle.yPos, bigCircle.getX(), bigCircle.getY());
-        if (d < redCircle.radius + circleDiameter / 2 + 15) {
+        let d = dist(bgCircle.xPos, bgCircle.yPos, bigCircle.getX(), bigCircle.getY());
+        if (d < bgCircle.radius + circleDiameter / 2 + 15) {
           overlapping = true;
           break;
         }
       }
     }
-    redCircles.push(redCircle);
+    bgCircles.push(bgCircle);
   }
 }
 
 function draw() {
   background(5, 89, 127, 5);
+  /* Lowered background opacity so that past frames can be seen, 
+  which enhances the animation's movement */
 
 
-    // Draw all small red circles
-    for (let redCircle of redCircles) {
-      redCircle.display();
+    // Draw all small background circles
+    for (let bgCircle of bgCircles) {
+      bgCircle.display();
     }
 
-  // draw lines
+  // draw pink wavy lines
+  // Individual code: added noise to the X coordinate so that it moves around 
   for (let t = 0; t < wavylineX.length; t++) {
     wavyLines(wavylineX[t] * noise(wavyLineNoise), wavylineY[t], 2, 244, 198, 226, 10);
   }
 
-  // draw inner lines
+  // draw blue wavy lines
+    // Individual code: added noise to the X coordinate so that it moves around 
   for (let t = 0; t < wavylineX.length; t++) {
     wavyLines(wavylineX[t], wavylineY[t] * noise(wavyLineNoise), 1, 134, 198, 226, 4.2);
   }
 
+  // draws particles in the background to create some texture
   particles();
 
+  // draws function for the circle outline animation
   circleMoving(10, 20);
 
   // Draw all big circles
@@ -266,6 +283,7 @@ function draw() {
     circle.display();
   } 
 
+  // updates the noise to create movement
   colourNoise = colourNoise + 0.01;
   wavyLineNoise = wavyLineNoise + 0.002;
   nestedRingNoise = nestedRingNoise + 0.01;
@@ -276,10 +294,11 @@ function windowResized() {
   resizeCanvas(windowHeight, windowHeight);
   circleDiameter = (windowHeight / 20) * 5.5; 
   smallCircleDiameter = windowHeight/90;
- // Recalculate circle diameter after resize
+ // Recalculate circle diameters after resize
   draw(); // Redraw circles to reflect the new dimensions
 }
 
+// function to draw pink particles 
 function particles() {
   for (let x = 0; x < width; x++) {
     stroke(255, 50, 150);
@@ -288,6 +307,8 @@ function particles() {
   }
 }
 
+/* Function to create a moving circle outline with easing 
+Gives the impression that the big circle is pulsing */
 function circleMoving(smallestCircle, biggestCircle) {
 
   if (frameCount % 60 == 0) {
